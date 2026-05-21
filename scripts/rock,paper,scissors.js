c28698789ab276f844381301018ef750468b0e81
njambi101 <grace.nganga@student.moringaschool.com>
2026-05-21 18:15:44 +0300

    saved

---
+++ scripts/rock,paper,scissors.js
@@ -0,0 +1,20 @@
+/*let i = 1;
+while (i <= 10) {
+    
+    console.log(i)
+    i ++
+}
+
+
+for( let i = 1; i <= 15; i++){
+    console.log(i)
+}
+*/
+const foods = [
+    'burger',
+    'pizza',
+    'cheese',
+]
+for(let i = 0;  i < foods.length; i++){
+    console.log(i)
+}
