20c1846e5dd5604b466c55ab91821ad3bbd22038
njambi101 <grace.nganga@student.moringaschool.com>
2026-05-22 15:32:09 +0300

    saved

--- scripts/rock,paper,scissors.js
+++ scripts/rock,paper,scissors.js
@@ -18,26 +18,50 @@ let nos = 0;
 
 let nums =[];
 
-for(let i = 0;  i < number.length; i++){
+/*for(let i = 0;  i < number.length; i++){
     let foo = number[i];
     nos += foo;
     nums.push(foo *2)
     }
  console.log(nums)  
- 
- const text = "abc";
-const repeated = text.repeat(3); // "abcabcabc"
-console.log(repeated)
+ */
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+    console.log(string)
+  }
+  return string;
+};
+repeatString('john', 7)
+
+const a = 4;
+let b = 'food';
+let s =''; 
+for (let i = 0; i < 7; i++){
+   // let c = a[i];
+    s += a;
+    console.log(s)
+
+}
 
 
-const repeatString = function(word,num) {
-    if(num > 0){
-        let repeates = word.repeat(num);
-        console.log(repeates) 
 
-    }else{
-        console.log('error')
-    }
+/*const repeatString = function(word,num) {
+   for(let i = 0; i < num; i++){
+    
+    
+   }
+   console.log(words)
 };
 
  repeatString('njambi', 6)
+    
+
+const name = 'fuufuu';
+const numm = 4;
+ for(let i = 0; i < 7; i++){
+    // let names = name[i] 
+    //console.log(names)
+ }*/ 
