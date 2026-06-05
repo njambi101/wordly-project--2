a3375008652d3b165879441b13926d9bb61f260d
njambi101 <grace.nganga@student.moringaschool.com>
2026-06-06 02:43:17 +0300

    saved

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,4 +1,4 @@
-function val(event){
+/*function val(event){
     if(event.key === 'Enter'){
         names();
     }
@@ -16,3 +16,61 @@ function names(){
     document.querySelector('.js-display').innerHTML = n;
 }
 
+function a(){
+    return 'how are you'
+}
+let d = a();
+console.log(d)
+document.querySelector('.js-display').innerHTML = a();
+*/
+ //let num =document.querySelector('.js-names')
+    //let n = num.value;
+function age(){
+   let num = document.querySelector('.js-names');
+    let n = num.value;
+    if (n < 18){
+        
+        document.querySelector('.js-display').innerHTML ='sorry you are not allowed';
+    }else{
+        document.querySelector('.js-display').innerHTML = 'you are allowed to enter';
+    }
+
+//document.querySelector('.js-display').innerHTML = n;
+}
+function sayHello(y) {
+    return y ;
+}
+function t(z){
+    sayHello()
+    return y + z;
+}
+let text = sayHello();
+
+let a = 1000;
+let c = 600;
+function fullYear(firstYear, lastyear = 10){
+    console.log('how are you');
+    return firstYear - lastyear;
+    
+}
+let p = fullYear(a,c) *1000;
+console.log(p)
+
+function checkAge(age) {
+  if (age < 18) {
+    return "Too young";
+  }
+  return "Access granted";
+}
+let w = checkAge(166);
+console.log(w)
+
+function resipe(h){
+    if (h === 'rice'){
+        return 'add some water'
+    }
+        return 'just add some salt'
+    
+}
+let r = resipe('rice');
+console.log(r)
